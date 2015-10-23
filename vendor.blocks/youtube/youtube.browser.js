1cc025e5a905be4f218483886397e59ce600d26c
teryaew <mitya.teryaew@gmail.com>
2015-10-24 00:24:02 +0300

    fix youtube default button

--- vendor.blocks/youtube/youtube.browser.js
+++ vendor.blocks/youtube/youtube.browser.js
@@ -16,7 +16,7 @@ modules.define(
                     this._width = this.params.width || '560';
                     this._height = this.params.height || '315';
                     this._playerVars = this.params.playerVars || {};
-                    this._button = this.params.button || {};
+                    this._button = this.params.button || false;
                     this._buttonSize = this.params.buttonSize || 'l';
 
                     BEMDOM.append(
@@ -97,7 +97,7 @@ modules.define(
                 return {
                     // update default sizes
                     block : 'youtube-button',
-                    mods : { theme : 'default', size : this._buttonSize },
+                    mods : { theme : 'default', size : 'l' },
                     content : [
                         '<svg height="100%" version="1.1" viewBox="0 0 68 48" width="100%">',
                             '<path class="ytp-large-play-button-bg" d="m .66,37.62 c 0,0 .66,4.70 2.70,6.77 2.58,2.71 5.98,2.63 7.49,2.91 5.43,.52 23.10,.68 23.12,.68 .00,-1.3e-5 14.29,-0.02 23.81,-0.71 1.32,-0.15 4.22,-0.17 6.81,-2.89 2.03,-2.07 2.70,-6.77 2.70,-6.77 0,0 .67,-5.52 .67,-11.04 l 0,-5.17 c 0,-5.52 -0.67,-11.04 -0.67,-11.04 0,0 -0.66,-4.70 -2.70,-6.77 C 62.03,.86 59.13,.84 57.80,.69 48.28,0 34.00,0 34.00,0 33.97,0 19.69,0 10.18,.69 8.85,.84 5.95,.86 3.36,3.58 1.32,5.65 .66,10.35 .66,10.35 c 0,0 -0.55,4.50 -0.66,9.45 l 0,8.36 c .10,4.94 .66,9.45 .66,9.45 z" fill="#1f1f1e" fill-opacity="0.9"></path>',
